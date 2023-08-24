b80de88c50a8b740c8e2007c34d85a9f8a068d3e
Ryan Finlay <ryan31@my.yorku.ca>
2023-08-24 03:06:06 -0400

    firebase testing

--- server/server.js
+++ server/server.js
@@ -7,6 +7,7 @@ const app = express();
 app.use(express.static(path.join(__dirname, '../public')));
 
 //Removes the need for .html in url
+//(No longer needed because of cleanUrls with firebase)
 app.use(async (req, res, next) => {
     const file = path.join(__dirname, '../public', req.path + '.html');
     try {
@@ -23,6 +24,7 @@ app.get('/play/:location', (req, res) => {
 });
 
 //If no file was found, then redirect to the home page
+//(No longer needed because of 404.html error page with firebase)
 app.use((req, res) => {
     res.redirect('/');
 });
