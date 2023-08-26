a3e335ce9732df7a3bed0542e28ff414f9ffa0fd
Ryan Finlay <ryan31@my.yorku.ca>
2023-08-26 16:53:41 -0400

    added cloud functions

--- functions/index.js
+++ functions/index.js
@@ -1,19 +1,49 @@
-/**
- * Import function triggers from their respective submodules:
- *
- * const {onCall} = require("firebase-functions/v2/https");
- * const {onDocumentWritten} = require("firebase-functions/v2/firestore");
- *
- * See a full list of supported triggers at https://firebase.google.com/docs/functions
- */
+const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+const express = require('express');
+const path = require('path');
 
-// const {onRequest} = require("firebase-functions/v2/https");
-// const logger = require("firebase-functions/logger");
+//If db is in emulation mode, initialize emulation db
+if (process.env.FUNCTIONS_EMULATOR) {
+    const databaseURL = `http://localhost:9000?ns=${process.env.GCLOUD_PROJECT}`;
+    admin.initializeApp({
+        databaseURL: databaseURL
+    });
+} else {
+    admin.initializeApp();
+}
 
-// Create and deploy your first functions
-// https://firebase.google.com/docs/functions/get-started
+const app = express();
 
-// exports.helloWorld = onRequest((request, response) => {
-//   logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+app.use(express.static(path.join(__dirname, '../public')));
+
+// Changes play location parameter from ?=... into /...
+app.get('/play/:location', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public', 'play.html'));
+});
+
+// Returns 5 locations
+// Call with estateguesser.ca/api/(location)
+app.get('/api/:location', (req, res) => {
+    /*
+    const location = req.params.location;
+    const db = admin.database();
+    const ref = db.ref("/toronto");
+
+    ref.once("value").then(snapshot => {
+        const data = snapshot.val();
+        res.send(data);
+    }).catch(error => {
+        console.error("Error reading from database:", error);
+        res.status(500).send(error);
+  });
+  */
+ res.send( req.params.location + " data here." );
+});
+
+// If no file was found, then redirect to the home page
+app.use((req, res) => {
+    res.redirect('/');
+});
+
+exports.app = functions.https.onRequest(app);
--- public/scripts/play.js
+++ public/scripts/play.js
@@ -4,3 +4,15 @@ const chosenLocation = segments[segments.length - 1];
 console.log("JS file loaded successfully! Location = " + chosenLocation);
 if (chosenLocation === "tor") window.location.href = "/";
 document.getElementById("location-name").textContent = chosenLocation;
+
+//Test read from test db
+fetch(`/api/toronto`)
+    .then(response => response.json())
+    .then(data => {
+        console.log("Here");
+        console.log(data); // This will log the data returned from your cloud function
+        // Handle the data as needed
+    })
+    .catch(error => {
+        console.error("Error fetching data:", error);
+    });
